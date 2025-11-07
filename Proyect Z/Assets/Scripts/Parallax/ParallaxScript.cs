1730cfea00dfbd76574dad0fdf152e222014fe9c
Javier <jmmu169@gmail.com>
2025-11-07 18:49:13 +0100

    Revert "Merge branch 'main' of https://github.com/iberiansecrets/Proyect-Z"
    This reverts commit 977b290685877fe1c1690626444d2112e3883157, reversing
    changes made to a5123cc1d7ea245e08b77c509bd782152be11632.

--- Proyect Z/Assets/Scripts/Parallax/ParallaxScript.cs
+++ Proyect Z/Assets/Scripts/Parallax/ParallaxScript.cs
@@ -1,57 +1,24 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class ParallaxScript : MonoBehaviour
 {
-    RectTransform rectTransform;
     Vector2 startPos;
-    Vector2 maxOffset;
 
-    [SerializeField] int moveModifier = 10;
-    [SerializeField] float smoothness = 2f;
+    [SerializeField] int moveModifier;
 
     private void Start()
     {
-        rectTransform = GetComponent<RectTransform>();
-
-        rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
-        rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
-        rectTransform.pivot = new Vector2(0.5f, 0.5f);
-        rectTransform.anchoredPosition = Vector2.zero;
-        startPos = Vector2.zero;
-
-        Canvas canvas = GetComponentInParent<Canvas>();
-        RectTransform canvasRect = canvas.GetComponent<RectTransform>();
-        CanvasScaler scaler = canvas.GetComponent<CanvasScaler>();
-
-        Vector2 imageSize = GetWorldRect(rectTransform).size;
-        Vector2 canvasSize = GetWorldRect(canvasRect).size;
-
-        float marginX = Mathf.Max(0, (imageSize.x - canvasSize.x) / 2f);
-        float marginY = Mathf.Max(0, (imageSize.y - canvasSize.y) / 2f);
-        maxOffset = new Vector2(marginX, marginY);
+        startPos = transform.position;
     }
 
     private void Update()
     {
         Vector2 pz = Camera.main.ScreenToViewportPoint(Input.mousePosition);
 
-        float targetX = (pz.x - 0.5f) * moveModifier;
-        float targetY = (pz.y - 0.5f) * moveModifier;
+        float posX = Mathf.Lerp(transform.position.x, startPos.x + (pz.x * moveModifier), 2f * Time.deltaTime);
+        float posY = Mathf.Lerp(transform.position.y, startPos.y + (pz.y * moveModifier), 2f * Time.deltaTime);
 
-        targetX = Mathf.Clamp(targetX, -maxOffset.x, maxOffset.x);
-        targetY = Mathf.Clamp(targetY, -maxOffset.y, maxOffset.y);
-
-        Vector2 newPos = Vector2.Lerp(rectTransform.anchoredPosition, new Vector2(targetX, targetY), smoothness * Time.deltaTime);
-        rectTransform.anchoredPosition = newPos;
-    }
-
-    Rect GetWorldRect(RectTransform rt)
-    {
-        Vector3[] corners = new Vector3[4];
-        rt.GetWorldCorners(corners);
-        float width = Vector3.Distance(corners[0], corners[3]);
-        float height = Vector3.Distance(corners[0], corners[1]);
-        return new Rect(corners[0].x, corners[0].y, width, height);
+        transform.position = new Vector3 (posX, posY, 0);
     }
 }
