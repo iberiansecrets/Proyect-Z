bbd64478afad61ec44bf5641a358ed28cda8608d
AntonioMB <anto293245@gmail.com>
2025-12-09 04:25:32 +0100

    Tutorial Listo + Ajustes y Videos

---
+++ Proyect Z/Assets/Scripts/MainMenu/TutorialManager.cs
@@ -0,0 +1,118 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine.Video;
+using TMPro;
+
+public class TutorialManager : MonoBehaviour
+{
+    public RawImage gifDisplay;
+    public Button leftArrowButton;
+    public Button rightArrowButton;
+
+    public TextMeshProUGUI titleText;
+    public TextMeshProUGUI descriptionText;
+    public TextMeshProUGUI pageNumberText;
+
+    public VideoPlayer videoPlayer;
+
+    [System.Serializable]
+    public class TutorialPage
+    {
+        public string pageTitle;
+        [TextArea]
+        public string pageDescription;
+        public VideoClip pageVideoClip;
+    }
+
+    public List<TutorialPage> tutorialPages;
+
+    private int currentPageIndex = 0;
+
+    void Start()
+    {
+        if (videoPlayer != null)
+        {
+            videoPlayer.isLooping = true;
+        }
+
+        rightArrowButton.onClick.AddListener(NextPage);
+        leftArrowButton.onClick.AddListener(PreviousPage);
+        UpdatePage(0);
+    }
+
+    public void NextPage()
+    {
+        if (currentPageIndex < tutorialPages.Count - 1)
+        {
+            currentPageIndex++;
+            UpdatePage(currentPageIndex);
+        }
+    }
+
+    public void PreviousPage()
+    {
+        if (currentPageIndex > 0)
+        {
+            currentPageIndex--;
+            UpdatePage(currentPageIndex);
+        }
+    }
+
+    private void UpdatePage(int newIndex)
+    {
+        if (newIndex < 0 || newIndex >= tutorialPages.Count)
+        {
+            Debug.LogError("Índice de página del tutorial fuera de límites: " + newIndex);
+            return;
+        }
+
+        currentPageIndex = newIndex;
+        TutorialPage current = tutorialPages[currentPageIndex];
+
+        int displayedPageNumber = currentPageIndex + 1;
+
+        // 1. Actualizar Textos
+        if (titleText != null)
+            titleText.text = current.pageTitle;
+
+        if (descriptionText != null)
+            descriptionText.text = current.pageDescription;
+
+        // 2. Actualizar GIF
+        if (videoPlayer != null)
+        {
+            videoPlayer.clip = current.pageVideoClip;
+
+            if (current.pageVideoClip != null)
+            {
+                // Si hay un clip, lo cargamos y reproducimos
+                videoPlayer.Prepare();
+                videoPlayer.Play();
+                gifDisplay.gameObject.SetActive(true);
+            }
+            else
+            {
+                // Si no hay clip (Pág 0 o Pág 7), paramos y ocultamos
+                videoPlayer.Stop();
+                gifDisplay.gameObject.SetActive(false);
+            }
+        }
+
+        if (pageNumberText != null)
+        {
+            pageNumberText.text = $"{displayedPageNumber}";
+        }
+
+        UpdateArrowButtons();
+    }
+
+    private void UpdateArrowButtons()
+    {
+        int lastPageIndex = tutorialPages.Count - 1;
+
+        leftArrowButton.gameObject.SetActive(currentPageIndex > 0);
+
+        rightArrowButton.gameObject.SetActive(currentPageIndex < lastPageIndex);
+    }
+}
